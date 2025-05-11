dac410ce8ecd5dfafebe6f2844f6b5de58de6b17
Shaline Davida <shalinedavida@gmail.com>
2025-05-11 08:06:55 +0300

    added files

--- index.js
+++ index.js
@@ -5,76 +5,76 @@
 // and you must use a prototype method canAccess(userRole) to return true or false, a method toggleFeature(flag) to enable or disable the 
 // feature, and simulate access attempts using if-else and switch statements for different roles.
 
-  //  function FeatureToggle(feature, isEnabled, userGroupAccess){
-  //   this.feature=feature;
-  //   this.isEnabled=isEnabled;
-  //   this.userGroupAccess=userGroupAccess;
-  //  }
+   function FeatureToggle(feature, isEnabled, userGroupAccess){
+    this.feature=feature;
+    this.isEnabled=isEnabled;
+    this.userGroupAccess=userGroupAccess;
+   }
 
-  //  FeatureToggle.prototype.canAccess= function(userRole){
-  //      return this.isEnabled && this.userGroupAccess.includes(userRole);
-  //  };
-  //  FeatureToggle.prototype.toggleFeature= function(flags){
-  //   if ( this.flag == flags){
-  //       return 'enabled'
-  //   }  else {
-  //       return 'disabled'
-  //   }
-  //  }
-  //  FeatureToggle.prototype.accessSimulation=function(userRole){
-  //  switch(userRole){
-  //   case "admin":
-  //       console.log(` Admins can access feature`);
-  //       break ;
-  //       case "betaTesters":
-  //        console.log(` Betatesters can access feature`);
-  //        break;
-  //        case"managers":
-  //        console.log(`Managers can access feature`);
-  //        break;
-  //        default :
-  //        break;    
+   FeatureToggle.prototype.canAccess= function(userRole){
+       return this.isEnabled && this.userGroupAccess.includes(userRole);
+   };
+   FeatureToggle.prototype.toggleFeature= function(flags){
+    if ( this.flag == flags){
+        return 'enabled'
+    }  else {
+        return 'disabled'
+    }
+   }
+   FeatureToggle.prototype.accessSimulation=function(userRole){
+   switch(userRole){
+    case "admin":
+        console.log(` Admins can access feature`);
+        break ;
+        case "betaTesters":
+         console.log(` Betatesters can access feature`);
+         break;
+         case"managers":
+         console.log(`Managers can access feature`);
+         break;
+         default :
+         break;    
         
-  //  }
-  //  }
-  //  const newFeature = new FeatureToggle('lightMode', 'true', ['admin','manager','student','betaTesters']);
-  //  console.log(newFeature);
-  //  console.log(newFeature.canAccess());
-  //  console.log(newFeature.toggleFeature());
-  //  console.log(newFeature.accessSimulation('betaTesters'));
+   }
+   }
+   const newFeature = new FeatureToggle('lightMode', 'true', ['admin','manager','student','betaTesters']);
+   console.log(newFeature);
+   console.log(newFeature.canAccess());
+   console.log(newFeature.toggleFeature());
+   console.log(newFeature.accessSimulation('betaTesters'));
    
   // 2.In a freelancer time-tracking platform, create a TimeLog constructor function with properties: freelancerName (string),
  //  projectDetails (object with name and hourlyRate), and logs (array of objects with date, hoursWorked), then add prototype 
 // methods to calculate total earnings, filter logs by date range, and determine if weekly hours exceed 40 using if-else logic.
 
-// function Timelog (freelancerName,projectDetails,logs){
-//     this.freelancerName=freelancerName;
-//     this.projectDetails=projectDetails;
-//     this.logs=logs;
+function Timelog (freelancerName,projectDetails,logs){
+    this.freelancerName=freelancerName;
+    this.projectDetails=projectDetails;
+    this.logs=logs;
 
     
-// }
-// Timelog.prototype.totalEarnings= function(){
-//       const earnings= this.hourlyRate * this.hoursWorked
-//      return earnings
-// }
+}
+Timelog.prototype.totalEarnings= function(){
+      const earnings= this.hourlyRate * this.hoursWorked
+     return earnings
+}
 // Timelog.prototype.dateRange= function(date){
  
 //    date.filter(dateString => {
 //     const date = new Date(dateString);
 //     return date >= startDate && date <= endDate;
  
-//   });
-//   if(this.date> 40){
-//     return exceed
-//   }else{
-//     return subceed
-//   }
+  // });
+  // if(this.date> 40){
+  //   return exceed
+  // }else{
+  //   return subceed
+  // }
 
-// }
-// const timelog = new Timelog('Sofia',{'name:Sof':'hourlyRate:20'},[{'date:5/2/2025':'hoursWorked:5'},{'date:6/2/2025':'hoursWorked:6'},{'date:7/2/2025':'hoursWorked:7'}])
-// console.log(timelog);
-// console.log(timelog.totalEarnings());
+
+const timelog = new Timelog('Sofia',{'name:Sof':'hourlyRate:20'},[{'date:5/2/2025':'hoursWorked:5'},{'date:6/2/2025':'hoursWorked:6'},{'date:7/2/2025':'hoursWorked:7'}])
+console.log(timelog);
+console.log(timelog.totalEarnings());
 // console.log(timelog.dateRange());
 
 
@@ -82,43 +82,43 @@
 // // 3.You are developing a startup’s order management system where an Order constructor function should contain customer 
 // // (object with name and email), items (array of objects with productName, quantity, and unitPrice), and status (string), 
 // // then implement prototype methods to compute total cost, update order status based on payment, and categorize order urgency using switch and conditional statements.
-// function Order(customer,items,status){
-//     this.customer=customer;
-//     this.items=items;
-//     this.status=status;
+function Order(customer,items,status){
+    this.customer=customer;
+    this.items=items;
+    this.status=status;
 
-// }
-// Order.prototype.totalCost=function(items){
-//     return this.items.reduce((cost,item) => cost + item.quantity * item.unitPrice,0);
+}
+Order.prototype.totalCost=function(items){
+    return this.items.reduce((cost,item) => cost + item.quantity * item.unitPrice,0);
   
-//     };
+    };
    
-// Order.prototype.orderStatus=function(receivedPayment){
-//    if (receivedPayment){
-//     this.status = 'paid';
-//    }else {
-//     this.status = 'pending';
-//    }
-//   }
+Order.prototype.orderStatus=function(receivedPayment){
+   if (receivedPayment){
+    this.status = 'paid';
+   }else {
+    this.status = 'pending';
+   }
+  }
 
-// Order.prototype.orderUrgency=function(){
-//             switch(this.status){
-//               case 'paid':
-//                console.log('low urgency');
-//                break;
-//               case 'pending':
-//                 console.log('high urgency');
-//                 break;
-//                 default:
-//                   console.log('no uregncy');
+Order.prototype.orderUrgency=function(){
+            switch(this.status){
+              case 'paid':
+               console.log('low urgency');
+               break;
+              case 'pending':
+                console.log('high urgency');
+                break;
+                default:
+                  console.log('no uregncy');
                      
-//             }
-// }
-// const order = new Order({'name:Fayah':'email:fayahtess@gmail.com'},[{'productName':'shoes','quantity':5,'unitPrice':600},{'productName':'shoes','quantity':4,'unitPrice':500}],'available')
-// console.log(order);
-// console.log(order.totalCost());
-// console.log(order.orderStatus('paid'));
-// console.log(order.orderUrgency());
+            }
+}
+const order = new Order({'name:Fayah':'email:fayahtess@gmail.com'},[{'productName':'shoes','quantity':5,'unitPrice':600},{'productName':'shoes','quantity':4,'unitPrice':500}],'available')
+console.log(order);
+console.log(order.totalCost());
+console.log(order.orderStatus('paid'));
+console.log(order.orderUrgency());
 
 
 
